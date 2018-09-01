9d0975d077f3c04f0b6768ddc208edefd63f5f8a
xiaobailalala <1051973784@qq.com>
2018-09-01 09:59:35 +0800

    后台管理-管理员，职业，食谱分类完成

--- src/main/java/com/smxy/recipe/controller/RecipeController.java
+++ src/main/java/com/smxy/recipe/controller/RecipeController.java
@@ -9,13 +9,19 @@
  */
 package com.smxy.recipe.controller;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/recipe")
 public class RecipeController {
 
-
+    @RequiresPermissions("recipe:select")
+    @GetMapping("/add")
+    public String add(){
+        return "admin/recipe/add";
+    }
 
 }
