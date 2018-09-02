e13f13d5a58a0ff48dd96357ed9302cd69817cac
liufei <894899162@qq.com>
2018-09-02 21:46:57 +0800

    刘飞的测试（6）

--- src/main/resources/static/src/js/recipe/add.js
+++ src/main/resources/static/src/js/recipe/add.js
@@ -1,3 +1,21 @@
 $(function(){
+    (function Add(){
+        $('#browser').click(function(){
+            $('#fCover').click();
+        });
+        $('#fCover').change(function(){
+            if (this.files[0]){
+                onImg(this.files[0]);
+            }
+        });
+        function addd(){
 
+        }
+        function onImg(data) {
+            var fs = new FileReader();
+            fs.onload = function () {
+                var image='<img src="'+fs.result+'" style="height: 100%;width: 100%;" alt="cover">';
+            }
+        }
+    }());
 });
