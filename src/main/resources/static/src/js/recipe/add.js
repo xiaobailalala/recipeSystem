fbfc0527900bb23a029d48bd4b13fde6857cc939
xiaobailalala <1051973784@qq.com>
2018-09-02 19:57:56 +0800

    测试，赵培贤提交的代码（4）

--- src/main/resources/static/src/js/recipe/add.js
+++ src/main/resources/static/src/js/recipe/add.js
@@ -14,7 +14,7 @@ $(function(){
             fs.onload = function () {
                 var image='<img src="'+fs.result+'" style="height: 100%;width: 100%;" alt="cover">';
                 $('#imgCont').html(image);
-                function asd(){}
+                function aaaaaaa(){}
             }
         }
     }());
