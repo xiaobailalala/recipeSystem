db40b7b286cbfeafe1e3c30465a4133fab74c18f
yangyihui <1270730209@qq.com>
2018-09-02 20:02:35 +0800

    测试，在add.js中改变了代码

--- src/main/resources/static/src/js/recipe/add.js
+++ src/main/resources/static/src/js/recipe/add.js
@@ -14,7 +14,8 @@ $(function(){
             fs.onload = function () {
                 var image='<img src="'+fs.result+'" style="height: 100%;width: 100%;" alt="cover">';
                 $('#imgCont').html(image);
-                function aaaaaaa(){}
+
+                function aaaaaaaaaffff() {}
             }
         }
     }());
